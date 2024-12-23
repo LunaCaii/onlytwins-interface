815a428cf84cc6e4330d6cf916ee46742c0291e2
onlytwins <dev@onlytwins.ai>
2024-12-24 00:57:03 +0800

    feat: :sparkles: refactor screen layout for better mobile and desktop display

--- app/components/screen.tsx
+++ app/components/screen.tsx
@@ -7,17 +7,39 @@ export function Screen() {
   ]
 
   return (
-    <ul className="main-layout flex">
-      {screens.map((screen, index) => (
-        <li
-          key={screen}
-          className={`relative ${index % 2 === 0 ? 'translate-y-24' : ''} ${
-            index > 0 ? '-ml-20' : ''
-          }`}
-        >
-          <img src={screen} alt="" />
-        </li>
-      ))}
-    </ul>
+    <div className="main-layout">
+      <ul className="lg:flex hidden">
+        {screens.map((screen, index) => (
+          <li
+            key={screen}
+            className={`relative h-fit lg:mt-0 ${
+              index % 2 === 0 ? '!mt-20' : ''
+            } ${index > 0 ? 'lg:-ml-20' : ''}`}
+          >
+            <img src={screen} alt="" className="block w-full h-auto" />
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile */}
+      <div className="lg:hidden flex">
+        <ul>
+          {screens.slice(0, screens.length / 2).map((screen, index) => (
+            <li key={index} className="mt-32 first:mt-0">
+              <img src={screen} alt="" className="block w-fit h-fit" />
+            </li>
+          ))}
+        </ul>
+        <ul className="mt-24 -ml-20">
+          {screens
+            .slice(screens.length / 2, screens.length)
+            .map((screen, index) => (
+              <li key={index} className="mt-32 max-w-full">
+                <img src={screen} alt="" className="block w-full" />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
   )
 }
