9f668d94d010eb8568a44550b9280fe609a16ece
onlytwins <dev@onlytwins.ai>
2024-12-23 15:21:44 +0800

    feat: :sparkles: add Google Tag Manager integration to the layout component

--- app/root.tsx
+++ app/root.tsx
@@ -4,26 +4,46 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
-import { Header } from "~/components/header";
+} from '@remix-run/react'
+import { useEffect } from 'react'
+import type { LinksFunction } from '@remix-run/node'
+import { Header } from '~/components/header'
 
-import "./tailwind.css";
+import './tailwind.css'
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'anonymous',
   },
   {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
-];
+]
+const GTM_ID = 'GTM-5JXGXGHG'
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // loading Google Tag Manager script
+    const script = document.createElement('script')
+    script.async = true
+    script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`
+    document.head.appendChild(script)
+
+    // noscript
+    const noscript = document.createElement('noscript')
+    noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+    document.body.appendChild(noscript)
+
+    return () => {
+      document.head.removeChild(script)
+      document.body.removeChild(noscript)
+    }
+  }, [])
+
   return (
     <html lang="en">
       <head>
@@ -39,9 +59,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
